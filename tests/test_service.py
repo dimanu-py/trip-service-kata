47afe54e58f3efc2b04613350084bd0d7fa9600a
dimanu-py <diegomtz126@gmail.com>
2024-07-03 16:49:03 +0200

    test(service): user can't get trips if is not logged in

--- tests/test_service.py
+++ tests/test_service.py
@@ -1,4 +1,19 @@
+import unittest
+
+from src.trip_service_kata.exceptions import UserNotLoggedInException
+from src.trip_service_kata.service import TripService
 
 
-class TestTripService:
-    pass
+class SeamTripService(TripService):
+    
+    @staticmethod
+    def logged_user():
+        return None
+
+
+class TestTripService(unittest.TestCase):
+
+    def test_user_cannot_get_trips_if_is_not_logged_in(self) -> None:
+        trip_service = SeamTripService()
+        with self.assertRaises(UserNotLoggedInException):
+            trip_service.get_trips_by_user(None)
