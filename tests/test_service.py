8f8408f0d7f41f4194fb1fdeebda147b6682e28e
dimanu-py <diegomtz126@gmail.com>
2024-07-03 18:50:31 +0200

    test(service): user gets no trips if it not friend of user to be consulted

--- tests/test_service.py
+++ tests/test_service.py
@@ -2,6 +2,7 @@ import unittest
 
 from src.trip_service_kata.exceptions import UserNotLoggedInException
 from src.trip_service_kata.service import TripService
+from src.trip_service_kata.trip import Trip
 from src.trip_service_kata.user import User
 
 
@@ -17,6 +18,8 @@ class SeamTripService(TripService):
 class TestTripService(unittest.TestCase):
     GUEST_USER = None
     LOGGED_USER = User()
+    GENERIC_USER = User()
+    GREECE = Trip()
 
     def setUp(self):
         self.application_user = self.LOGGED_USER
@@ -27,3 +30,12 @@ class TestTripService(unittest.TestCase):
 
         with self.assertRaises(UserNotLoggedInException):
             self.trip_service.get_trips_by_user(self.GUEST_USER)
+
+    def test_user_gets_no_trips_if_is_not_friend(self) -> None:
+        stranger = User()
+        stranger.add_friend(self.GENERIC_USER)
+        stranger.add_trip(self.GREECE)
+
+        trips = self.trip_service.get_trips_by_user(stranger)
+
+        self.assertEqual(trips, [])
