34e56985574a0689e4a6673720bcb9dc751d8624
dimanu-py <diegomtz126@gmail.com>
2024-07-03 19:58:01 +0200

    feat(builder): create UserBuilder to manage User creation easily

---
+++ src/trip_service_kata/user_builder.py
@@ -0,0 +1,25 @@
+from src.trip_service_kata.trip import Trip
+from src.trip_service_kata.user import User
+
+
+class UserBuilder:
+
+    def __init__(self):
+        self.trips: list[Trip] = []
+        self.friends: list[User] = []
+
+    def friends_with(self, friends: list[User]) -> "UserBuilder":
+        self.friends = friends
+        return self
+
+    def with_trips_to(self, trips: list[Trip]) -> "UserBuilder":
+        self.trips = trips
+        return self
+
+    def build(self) -> User:
+        user = User()
+        for friend in self.friends:
+            user.add_friend(friend)
+        for trip in self.trips:
+            user.add_trip(trip)
+        return user
--- tests/test_service.py
+++ tests/test_service.py
@@ -4,6 +4,7 @@ from src.trip_service_kata.exceptions import UserNotLoggedInException
 from src.trip_service_kata.service import TripService
 from src.trip_service_kata.trip import Trip
 from src.trip_service_kata.user import User
+from src.trip_service_kata.user_builder import UserBuilder
 
 
 class SeamTripService(TripService):
@@ -35,18 +36,19 @@ class TestTripService(unittest.TestCase):
             self.trip_service.get_trips_by_user(self.GUEST_USER)
 
     def test_user_gets_no_trips_if_is_not_friend(self) -> None:
-        stranger = User()
-        stranger.add_friend(self.GENERIC_USER)
-        stranger.add_trip(self.GREECE)
+        stranger = (UserBuilder()
+                    .friends_with([self.GENERIC_USER])
+                    .build())
 
         trips = self.trip_service.get_trips_by_user(stranger)
 
         self.assertEqual(trips, [])
 
     def test_user_gets_trips_of_friend(self) -> None:
-        friend = User()
-        friend.add_friend(self.logged_user)
-        friend.add_trip(self.GREECE)
+        friend = (UserBuilder()
+                  .friends_with([self.logged_user])
+                  .with_trips_to([self.GREECE])
+                  .build())
 
         trips = self.trip_service.get_trips_by_user(friend)
 
